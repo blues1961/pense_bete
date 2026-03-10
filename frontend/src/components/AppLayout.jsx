b7e988f3737d65ea211c9d69be2b6507958fd53f
blues1961 <blues1961@gmail.com>
2026-03-10 10:20:21 -0400

    Align header branding with password manager

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 import { AuthError, createItem } from "../api";
-import monSiteSymbol from "../assets/mon-site-symbol.png";
+import monSiteLogo from "../assets/mon-site-logo.png";
 import QuickCaptureForm from "./QuickCaptureForm";
 import SidebarNav from "./SidebarNav";
 import ThemeToggle from "./ThemeToggle";
@@ -40,11 +40,8 @@ export default function AppLayout({
       <header className="topbar">
         <div className="topbar__inner">
           <div className="brand">
-            <img src={monSiteSymbol} alt="mon-site.ca" className="brand__logo brand__logo--symbol" />
-            <div>
-              <p className="brand__eyebrow">mon-site.ca</p>
-              <strong className="brand__name">Pense-bête</strong>
-            </div>
+            <img src={monSiteLogo} alt="mon-site.ca" className="brand__logo" />
+            <strong className="brand__name">Pense-bête</strong>
           </div>
           <div className="topbar__right">
             <ThemeToggle theme={theme} onChange={onThemeChange} />
